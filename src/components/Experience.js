36bc4b88aadd61de7c5847fc3cc9374ea61675ad
GaneshTowar <47410765+GaneshTowar@users.noreply.github.com>
2024-01-23 15:31:13 +0530

    font changes issue fix

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Css/Experience.css";
-import background from "./Images/background.jpg"
 import "./Css/Project.css"
 const Experience = () => {
   const Experience = [
@@ -17,7 +16,7 @@ const Experience = () => {
   ];
 
   return (
-    <div className="h-fit py-6  bg-cover bg-blend-lighten text-stroke text-white" >
+    <div className="h-fit py-6  bg-cover bg-blend-lighten text-white" >
       <h2 className="font-semibold text-4xl text-center sm:pt-12" id="#exp">
         Experience
       </h2>
@@ -27,17 +26,17 @@ const Experience = () => {
           key={index}
         >
           <div className="flex m-2 justify-center ">
-            <p className="w-fit bg-black rounded-2xl p-3 font-medium border-black border-b-2 border-4 hover:bg-gradient-to-r hover:border-2 from-white to-slate-400 cursor-pointer">
+            <p className="w-fit text-stroke text-white bg-black rounded-2xl p-3 font-medium border-black border-b-2 border-4 hover:bg-gradient-to-r hover:border-2 from-white to-slate-400 cursor-pointer">
               {exp.companyname}
             </p>
           </div>
           <div className="flex m-2 justify-center  ">
-            <p className="w-fit bg-white rounded-2xl p-1 px-4 font-normal border-black border-2 hover:border-2">
+            <p className="w-fit bg-white rounded-2xl text-black p-1 px-4 font-normal border-black border-2 hover:border-2">
               {exp.job}
             </p>
           </div>
           <div className="flex m-2 justify-center ">
-            <p className="w-fit bg-white rounded-2xl p-1 px-4 font-light border-black text-sm border-b-2 border-2 hover:border-2 ">
+            <p className="w-fit bg-white rounded-2xl text-black p-1 px-4 font-light border-black text-sm border-b-2 border-2 hover:border-2 ">
               {exp.date}
             </p>
           </div>
